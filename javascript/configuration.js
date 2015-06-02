495931ffd3e5de1bfc596178d1084765b95ad5e1
tloubrieu <tloubrie@ifremer.fr>
2015-06-02 18:13:59 +0200

    updated configuration for nominal environment

--- javascript/configuration.js
+++ javascript/configuration.js
@@ -1,6 +1,6 @@
 // production
-//var SNANNY_API = "/api/rest";
+var SNANNY_API = "/api/rest";
 // validation
-var SNANNY_API = "http://isi.ifremer.fr/snanny-api/rest";
+//var SNANNY_API = "http://isi.ifremer.fr/snanny-api/rest";
 // development
 //var SNANNY_API = "http://ubisi54.ifremer.fr:8080/api/rest";
