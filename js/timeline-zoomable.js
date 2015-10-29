aa36d769546a33fc330aa1cfaee62d6608e69281
Liberge <sliberge@asi.fr>
2015-10-29 17:55:13 +0100

    ajout des boutons de zoom sur la timeline

--- js/timeline-zoomable.js
+++ js/timeline-zoomable.js
@@ -9,6 +9,9 @@ var timelineArea;
 var timelineXAxis;
 var timelineXTranslate = 0;
 var timelineXScale = 1;
+var svg;
+
+var zoom
 var 	margin = {
 		top : 20,
 		right : 20,
@@ -21,7 +24,7 @@ function initializeTimeline(data) {
 	var container = d3.select('#timeline');
 	timelineWidth  = container.node().offsetWidth - margin.left - margin.right;
 	timelineHeight = container.node().offsetHeight - margin.top - margin.bottom - /* scroll */15;
-
+	
 	// Compute X axis
 	var current_first_time = Number.MAX_VALUE;
 	var timeExtent = d3.extent(data, function(d) {
@@ -41,23 +44,10 @@ function initializeTimeline(data) {
 	.domain(timeExtent)
 	;
 	
-//	var customTimeFormat = d3.time.format.multi([
-//	                                             [".%L", function(d) { return d.getMilliseconds(); }],
-//	                                             [":%S", function(d) { return d.getSeconds(); }],
-//	                                             ["%I:%M", function(d) { return d.getMinutes(); }],
-//	                                             ["%I %p", function(d) { return d.getHours(); }],
-//	                                             ["%a %d", function(d) { return d.getDay() && d.getDate() != 1; }],
-//	                                             ["%b %d", function(d) { return d.getDate() != 1; }],
-//	                                             ["%B", function(d) { console.log(d.getMonth()); return d.getMonth(); }],
-//	                                             ["%Y", function() { return true; }]
-//	                                           ]);
-
-	
 	timelineXAxis = d3.svg.axis()
 	.scale(timelineX)
 	.orient('bottom')
 	.tickPadding(8)	
-//	.tickFormat(customTimeFormat)
 	;
 	
 	
@@ -72,17 +62,22 @@ function initializeTimeline(data) {
 	;
 
 	var changeRequest;
+	zoom = d3.behavior.zoom().x(timelineX)
+							.y(timelineY)
+							.scaleExtent([1, timelineWidth])
+							.on("zoom",  timelineZoom)
+							.on('zoomend', function(){
+								clearTimeout(changeRequest);
+								timelineSelection = timelineX.domain();
+								changeRequest = setTimeout(function(){
+									getObservationsCount();
+									getObservations();
+								}, 500);		
+							});
 	timelineSvg = container.append('svg')
-	.attr('width', timelineWidth * timelineScale + margin.left + margin.right)
-	.attr('height', timelineHeight + margin.top + margin.bottom)
-	.call(d3.behavior.zoom().x(timelineX).y(timelineY).scaleExtent([1, timelineWidth]).on("zoom",  timelineZoom).on('zoomend', function(){
-		clearTimeout(changeRequest);
-		timelineSelection = timelineX.domain();
-		changeRequest = setTimeout(function(){
-			getObservationsCount();
-			getObservations();
-		}, 500);		
-	}))
+							.attr('width', timelineWidth * timelineScale + margin.left + margin.right)
+							.attr('height', timelineHeight + margin.top + margin.bottom)
+							.call(zoom)
 	;
 
 	var context = timelineSvg.append('g')
@@ -115,48 +110,105 @@ function initializeTimeline(data) {
 	.attr('class', 'x axis')
 	.attr('transform', 'translate(0,' + (timelineHeight + 21) + ')')
 	.call(timelineXAxis)
-//	.selectAll("text")
-//    .attr("transform", "rotate(90)")
-//    .style("text-anchor", "start")
     ;
 
 	setTimelineAll(data);
+	
+	svg = d3.select("body").append("svg").append("g"); 
+	initAppControls();
 }
+function initAppControls() {
+	
 
+	svg.selectAll(".zoomBtn")
+    .call(zoom)
+    .call(zoom.event);
+	console.log(zoom);
+	d3.selectAll('.zoomBtn').on('click', function(){
+		var currentZoom = zoom.scale();		
+		var scale = zoom.scale(),
+	        extent = zoom.scaleExtent(),
+	        translate = zoom.translate(),
+	        x = translate[0], y = translate[1],
+	        factor = (this.id === 'zoomIn') ? 1.2 : 1/1.2,	        		
+	        target_scale = scale * factor;
+	    // If we're already at an extent, done
+	    if (target_scale === extent[0] || target_scale === extent[1]) { return false; }
+	    // If the factor is too much, scale it down to reach the extent exactly
+	    var clamped_target_scale = Math.max(extent[0], Math.min(extent[1], target_scale));
+	    if (clamped_target_scale != target_scale){
+	        target_scale = clamped_target_scale;
+	        factor = target_scale / scale;
+	    }
+
+	    // Center each vector, stretch, then put back
+	    center = [timelineWidth / 2, timelineHeight / 2];
+	    x = (x - center[0]) * factor + center[0];
+	    y = (y - center[1]) * factor + center[1];
+	 
+	
+	 // Transition to the new view over 350ms
+	    d3.transition().duration(350).tween("zoom", function () {
+	        var interpolate_scale = d3.interpolate(scale, target_scale),
+	            interpolate_trans = d3.interpolate(translate, [x,y]);
+	        timelineXTranslate = translate[0];
+	        timelineXScale = target_scale;
+	        
+	        return function (t) {
+	            zoom.scale(interpolate_scale(t))
+	                .translate(interpolate_trans(t));
+	            timelineZoom();
+	            svg.call(zoom.event);
+	        };
+	    });
+	});
+}
 function timelineZoom() {
 
-	// recupÈration de la transformation
-	timelineXTranslate = d3.event.translate[0];	
-	timelineXScale = d3.event.scale;
-	applyZoom();
+	// recup√©ration de la transformation
+	if(d3.event !==null){
+		timelineXTranslate = d3.event.translate[0];	
+		timelineXScale = d3.event.scale;
+		applyZoom();
+		
+	}	else{
+		// r√©cup√©ration des donn√©es 
+		var container = d3.select('#timeline');
+		var contextArea   = container.select('svg').select('g').select("path.area");	
+		var dataArray = contextArea.data();
+		var data = dataArray[0];
+		// recalcul de la zone
+		setTimeline(data);
+		var context   = container.select('svg').select('g');
+		container.select('svg').select(".x.axis").call(timelineXAxis);
+	//	context.select("path.areaAll").attr("transform", "scale(" + timelineXScale + ", 1)");
+	}
+	
 }
 
 function applyZoom(){
 	
-	// rÈcupÈration des donnÈes 
+	// r√©cup√©ration des donn√©es 
 	var container = d3.select('#timeline');
-	var context   = container.select('svg').select('g').select("path.area");	
-	var dataArray = context.data();
+	var contextArea   = container.select('svg').select('g').select("path.area");	
+	var dataArray = contextArea.data();
 	var data = dataArray[0];
-	// recalcul de la zonr
+	// recalcul de la zone
 	setTimeline(data);
-	setZoom(timelineXTranslate, timelineXScale);
-	
+	setZoom(timelineXTranslate, timelineXScale);	
 }
 
 function setZoom(translate, scale){
 	var container = d3.select('#timeline');
 	var context   = container.select('svg').select('g');
-	context.select("path.areaAll").attr("transform", "translate(" + translate + ",0)scale(" + scale + ", 1)");
 	container.select('svg').select(".x.axis").call(timelineXAxis);
-	
+	context.select("path.areaAll").attr("transform", "translate(" + translate + ",0)scale(" + scale + ", 1)");
+
 }
 
-function setTimelineAll(data) {
-	
+function setTimelineAll(data) {	
 	var container = d3.select('#timeline');
-	var context   = container.select('svg').select('g');
-	//setTimeLineArea(data);	
+	var context   = container.select('svg').select('g');		
 	context
 	.append("path")
 	.datum(data)
@@ -169,15 +221,13 @@ function setTimeline(data) {
 	var context   = container.select('svg').select('g');
 	context.selectAll('path.area').remove();
 	setTimeLineArea(data);
-
 	context
 	.append("path")
 	.datum(data)
 	.attr("class", "area")
-	.attr("d", timelineArea);
-	
+	.attr("d", timelineArea);	
 }
-	 //context.select("path.area").attr("transform", "translate(" + timelineXTranslate + ",0)scale(" + timelineXScale + ", 1)");
+	
 
 function setTimeLineArea(data){
 	var valueExtent = d3.extent(data, function(d) {
@@ -188,3 +238,4 @@ function setTimeLineArea(data){
 	.domain(valueExtent)
 	;
 }
+
