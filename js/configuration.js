8db54ced3fc5cfc38fb2fae7d820eef222c66ef1
tloubrieu <tloubrie@ifremer.fr>
2016-04-07 19:30:53 +0200

    new configuration for datacloud

--- js/configuration.js
+++ js/configuration.js
@@ -1,12 +1,14 @@
+//// INDEX API ////
+//// must be on same server as portal ////
 // production
-//var SNANNY_API = "/api/rest";
+//var SNANNY_HOST = 
 // validation
-//var SNANNY_API = "http://isi.ifremer.fr/snanny-api/rest";
+//var SNANNY_HOST = "http://isi.ifremer.fr/snanny-indexIO-api";
 // development
-//var SNANNY_API = "http://ubisi54.ifremer.fr:8080/api/rest";
+var SNANNY_HOST = "http://ubisi54.ifremer.fr/snanny-indexIO-api";
 // local
+//var SNANNY_HOST = "http://localhost.ifremer.fr:8080/api";
 
-var SNANNY_HOST = "http://localhost.ifremer.fr:8080/api";
 var SNANNY_API = SNANNY_HOST+"/rest";
 
 
@@ -14,5 +16,11 @@ var MAP_RESOURCES = SNANNY_API+"/obs/synthetic/map";
 var TIMELINE_RESOURCES = SNANNY_API+"/obs/synthetic/timeline";
 var OBSERVATIONS_RESOURCES = SNANNY_API+"/obs";
 
-var DATA_ACCESS_URI = "http://localhost:8888/owncloud/index.php/apps/snannyowncloudapi/";
+//// OWNCLOUD /////
+var OWNCLOUD_HOST = "http://visi-snanny-datacloud.ifremer.fr/owncloud"
+//var OWNCLOUD_HOST = "localhost:8888/owncloud"
+
+var DATA_ACCESS_URI = OWNCLOUD_HOST + "/index.php/apps/snannyowncloudapi/";
+
+//// CAS Authentication ////
 var CAS_URI = "https://auth.ifremer.fr/";
